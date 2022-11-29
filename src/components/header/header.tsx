2cee39951b575068867f4cc2c68c959f6071efec
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-11-29 11:15:35 +0200

    Feature: create header
    Signed-off-by: Alex Bezborodov <alexbezborodov23@gmail.com>

---
+++ src/components/header/header.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from "react";
+
+import MovieFilterIcon from "@mui/icons-material/MovieFilter";
+import { Typography } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { logout } from "../../store/action_creators";
+
+export const Header: FC<any> = ({ currentUser }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const TempRegistration = localStorage.getItem("tempRegistration");
+  const isContinueReg = TempRegistration
+    ? "Continue registration"
+    : "Go to register";
+  console.log("current user", currentUser);
+
+  const isEmpty = (obj: any) => {
+    return Object.keys(obj).length === 0;
+  };
+  const btnName = isEmpty(currentUser) ? isContinueReg : "Log out";
+
+  const handleLogout = () => {
+    navigate("/");
+    dispatch(logout());
+  };
+
+  const handleClick = () => {
+    isEmpty(currentUser) ? navigate("/signup") : handleLogout();
+  };
+
+  return (
+    <AppBar position="sticky" sx={{ background: "#000000" }}>
+      <Toolbar>
+        <Box display="flex" alignItems="center" onClick={() => navigate("/")}>
+          <MovieFilterIcon />
+          <Typography sx={{ marginX: 1 }}> MoviesDB</Typography>
+        </Box>
+
+        <Box sx={{ flexGrow: 1 }} />
+        <Button color="inherit" onClick={handleClick}>
+          {btnName}
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
--- src/components/movies/movies.tsx
+++ src/components/movies/movies.tsx
@@ -3,8 +3,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import { MoviesDBStore } from "../../interfaces";
+import { Header } from "../header";
 
 export const Movies = () => {
   const currentUser = useSelector((state: MoviesDBStore) => state.currentUser);
-  return <div>movies</div>;
+  console.log("currentUser", currentUser);
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+    </div>
+  );
 };
